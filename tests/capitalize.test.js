b1c5abd3ec72794435e894563615fc155cbda1f6
Tyler <57014233+tyleraj93@users.noreply.github.com>
2024-09-18 14:35:18 -0400

    Add test file with first test

---
+++ tests/capitalize.test.js
@@ -0,0 +1,12 @@
+import capitalize from "../src/capitalize";
+
+test("capitalizes the first letter of a string", () => {
+    expect(capitalize("hello")).toBe("Hello");
+});
+
+test("throws an error if input is not a string", () => {
+    expect(() => capitalize(123)).toThrow("Input must be a string");
+    expect(() => capitalize(null)).toThrow("Input must be a string");
+    expect(() => capitalize({})).toThrow("Input must be a string");
+    expect(() => capitalize([])).toThrow("Input must be a string");
+});
