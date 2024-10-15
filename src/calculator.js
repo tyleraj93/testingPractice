f5539fcb542423c700c31ed74ed23bb327cd8a4d
Tyler <57014233+tyleraj93@users.noreply.github.com>
2024-10-15 16:23:31 -0400

    Build calculator class

---
+++ src/calculator.js
@@ -0,0 +1,17 @@
+export default {
+    add: function (a, b) {
+        return a + b;
+    },
+
+    subtract: function (a, b) {
+        return a - b;
+    },
+
+    multiply: function (a, b) {
+        return a * b;
+    },
+
+    divide: function (a, b) {
+        return a / b;
+    },
+}
