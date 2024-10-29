e9fe14a47efc05661f34501144960c9e90f7a746
Tyler <57014233+tyleraj93@users.noreply.github.com>
2024-10-29 16:14:00 -0400

    Add test for cipher function

--- tests/caesarCipher.test.js
+++ tests/caesarCipher.test.js
@@ -1,4 +1,4 @@
-import caesarCipher from "../src/caesarCipher";
+import {caesarCipher} from "../src/caesarCipher";
 
 test('should wrap from z to a', () => {
     expect(caesarCipher("xyz", 3)).toBe("abc");
@@ -9,5 +9,10 @@ test('should preserve case', () => {
 });
 
 test('should keep non-alphabetic characters unchanged', () => {
-    expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog");
+    expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
 });
+
+test("should throw an error if input is incorrect format", () => {
+    expect(() => caesarCipher(123, 3)).toThrow(`Word must be in string format ie. "word"`);
+    expect(() => caesarCipher("catch", "3")).toThrow("Shift must be in int format ie. 3")
+})
