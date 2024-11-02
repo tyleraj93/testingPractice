89db0c029e0632a9c2e9076f1f20536bf09cb023
Tyler <57014233+tyleraj93@users.noreply.github.com>
2024-11-02 14:39:28 -0400

    Finish tests for analyzing array

--- tests/analyzeArray.test.js
+++ tests/analyzeArray.test.js
@@ -1,5 +1,10 @@
 import { analyzeArray } from "../src/analyzeArray";
 
+test("should throw an error if the input is not a list of one or more numbers", () => {
+    expect(() => analyzeArray([])).toThrow("Array must contain one or more numbers");
+    expect(() => analyzeArray([a])).toThrow("All elements in the array must be numbers")
+})
+
 test("should return an object with properties average, min, max, and length with values 4, 1, 8, 6", () => {
     expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
         average: 4,
